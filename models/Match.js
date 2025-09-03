1a5df7eb78d28728b32795f5e9ad383447576027
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-09-03 00:22:09 -1100

    Update Match.js

--- models/Match.js
+++ models/Match.js
@@ -2,13 +2,24 @@ const mongoose = require("mongoose");
 
 const MatchSchema = new mongoose.Schema({
   matchId: { type: String, unique: true },
-  league: String,
+  
+  // League info
+  leagueId: String,
+  leagueName: String,
+  leagueLogo: String,
+  leagueCountry: String,
+  
+  // Teams
   homeTeam: String,
   awayTeam: String,
-  homeLogo: String, 
-  awayLogo: String,  
+  homeLogo: String,
+  awayLogo: String,
+  
+  // Scores
   homeScore: Number,
   awayScore: Number,
+  
+  // Status & time
   status: String,
   date: Date,
 });
